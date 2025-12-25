c924f26f3f81f082dab313e839c1a8aa5d42fc3f
Nalu-devs <pordosol699@gmail.com>
2025-12-25 17:58:45 -0300

    ;

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 
 // createRoot busca um arquivo que tenha o id root e se ele encontrar esse elemento ele renderiza um  componente dentro da pagina no caso o App
 createRoot(document.getElementById('root')!).render(
   // StrictMode boa pratica
   <StrictMode>
-    <App />
+    
   </StrictMode>,
 )

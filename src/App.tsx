e20d61ac74a82d07bb1b99ea773a47d5d35504a5
Nalu-devs <pordosol699@gmail.com>
2025-12-26 12:01:23 -0300

    fim da aula 12

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+    console.log("Hello word");
+    //o tsx não retorna mais de um elemento, ele precisa estar com um elemento pai, por exemplo a div, porem pode ser <></> vazio
+    return (
+        <>
+            <h1>Olá mundo!</h1>
+            <p>Sejam bem vindos ao meu primeiro app pelo react</p>
+        </>);
+}
+// export default App; dessa forma o nome do import no main pode ser qualquer um por isso é melhor a outra opção abaixo:
+
+// export {App}; usuariamos assim para sites basicos com somente um export, porem é mais facil exportar logo a function
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import App from './App'; iriamos fazer assim caso usasemos o export default App
+import {App} from './App';
 
 // createRoot busca um arquivo que tenha o id root e se ele encontrar esse elemento ele renderiza um  componente dentro da pagina no caso o App
 createRoot(document.getElementById('root')!).render(
   // StrictMode boa pratica
   <StrictMode>
-    
+    <App/>
   </StrictMode>,
 )

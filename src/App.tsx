c5e966605b2efc110a34ac42cb34ed5d93b9cbbe
nalu-devs <pordosol699@gmail.com>
2025-12-29 21:09:57 -0300

    aula 20: criando a estrutura que usaremos para a criação do site usando html e css

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Heading } from './components/Heading';
-import { TimerIcon } from 'lucide-react';
 
 export function App() {
-    console.log("Hello word");
     //o tsx não retorna mais de um elemento, ele precisa estar com um elemento pai, por exemplo a div, porem pode ser <></> vazio
     return (
         <>
-            <Heading>
-                Ola mundo 1
-                <button>
-                    <TimerIcon/>
-                </button>
-            </Heading>
-            <p>Sejam bem vindos ao meu primeiro app pelo react</p>
+        <div className='container'>
+            <div className='content'>
+                <section>LOGO</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className='content'>
+                <section>MENU</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className='content'>
+                <section>FORM</section>
+            </div>
+        </div>
+
+        <div className='container'>
+            <div className='content'>
+                <section>FOOTER</section>
+            </div>
+        </div>
         </>);
 }
 // export default App; dessa forma o nome do import no main pode ser qualquer um por isso é melhor a outra opção abaixo:

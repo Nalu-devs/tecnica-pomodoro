3f72ada56f2cee0fe64563356ab24322199af4d8
Nalu-devs <pordosol699@gmail.com>
2026-01-01 17:45:19 -0300

    Aula 36: Aqui aprendi a como não deixar um link seguir o caminho dele, e como aplicar o onclick no link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,22 +1,32 @@
 import { HistoryIcon, HomeIcon, SettingsIcon, SunMoonIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+
+type EscolherTema = 'dark' | 'light';
 
 export function Menu(){
+
+    const [theme, setTheme] = useState<EscolherTema>('dark');
+
+    function mudarTheme(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>){
+        event.preventDefault(); //impede que a pagina va para o lugar dela, nesse caso impede que va para "#"
+        }
+
     return(
         <nav className={styles.menu}>
-            <a href="#" className={styles.menuLink}>
+            <a href="#" aria-label='Ir para a home' title="Ir para a home" className={styles.menuLink}>
                 <HomeIcon/>
             </a>
             
-            <a href="#" className={styles.menuLink}>
+            <a href="#" aria-label='Hitórico de navegação' title="Hitórico de navegação" className={styles.menuLink}>
                 <HistoryIcon/>
             </a>
             
-            <a href="#" className={styles.menuLink}>
+            <a href="#" aria-label='Configurações' title='Configurações' className={styles.menuLink}>
                 <SettingsIcon/>
             </a>
             
-            <a href="#" className={styles.menuLink}>
+            <a onClick={mudarTheme} href="#" aria-label='Mudar tema' title='Mudar tema' className={styles.menuLink}>
                 <SunMoonIcon/>
             </a>
         </nav>

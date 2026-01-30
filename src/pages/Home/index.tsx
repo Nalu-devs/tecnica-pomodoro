b3852abc355f3d254c92864df582a3c9cdac66fe
Nalu-devs <pordosol699@gmail.com>
2026-01-30 11:01:48 -0300

    criando parte de adicionar tarefas

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,9 @@
 import { Container } from "../../components/Container";
 import { CountDown } from "../../components/CountDown";
 import { MainForm } from "../../components/MainForm";
+import { TaskRegistration } from "../../components/TaskRegistration";
 import type { TaskStateModel } from "../../models/TaskStateModel";
+import type { TaskModel } from "../../models/TaskModel";
 import { MainTemplate } from "../../templates/MainTemplates";
 
 type HomeProps = {
@@ -9,11 +11,30 @@ type HomeProps = {
     setState: React.Dispatch<React.SetStateAction<TaskStateModel>>;
 }
 
-export function Home(props: HomeProps) {
+export function Home({ setState }: HomeProps) {
+    const handleAddTask = (newTask: Omit<TaskModel, 'id' | 'startDate' | 'completeDate' | 'interruptDate'>) => {
+        const task: TaskModel = {
+            ...newTask,
+            id: Date.now().toString(),
+            startDate: Date.now(),
+            completeDate: null,
+            interruptDate: null
+        };
+
+        setState(prevState => ({
+            ...prevState,
+            tasks: [...prevState.tasks, task]
+        }));
+    };
+
     return (
         <MainTemplate>
             <Container>
                 <CountDown />
+            </Container>
+
+            <Container>
+                <TaskRegistration onAddTask={handleAddTask} />
             </Container>
 
             <Container>

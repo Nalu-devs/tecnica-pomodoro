414863fffe3cc4a29489088d3973413b3808dc04
Nalu-devs <pordosol699@gmail.com>
2026-01-30 09:12:16 -0300

    ...

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,11 +1,9 @@
 import styles from './styles.module.css';
 import type React from "react";
-
 type Button = {
     icon: React.ReactNode;
     color?: 'green' | 'red';
 } & React.ComponentProps<'button'>;
-
 
 export function Button({icon, color="green" ,...rest}: Button){
     return (
